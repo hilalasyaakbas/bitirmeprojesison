5a34c57048f03295f753362cab3adc0e9955b74a
Hilal Asya <hilalasya@Hilal-MacBook-Air.local>
2026-05-28 00:16:41 +0300

    chore: clean up temporary scratch and test script files from the workspace

--- scratch_search.py
+++
@@ -1,7 +0,0 @@
-import requests
-API_KEY = "b7a102ffd063327aaa162b882ab6f386"
-BASE_URL = "https://api.themoviedb.org/3"
-query = "kayıp balık nemo"
-url = f"{BASE_URL}/search/movie?api_key={API_KEY}&language=tr-TR&query={query}"
-r = requests.get(url)
-print(r.json().get('results', [])[0].get('id'))
